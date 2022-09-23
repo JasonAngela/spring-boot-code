35f20c14fdba171896cc94c1a2a68ea6f0b15707
“huqilang” <huqilang@itg.com.cn>
2022-09-23 10:55:16 +0800

    map reduce

---
+++ day-test/src/main/java/com/qilang/test/Simple.java
@@ -0,0 +1,15 @@
+package com.qilang.test;
+
+import lombok.Data;
+
+/**
+ * @author hql
+ */
+@Data
+public class Simple {
+
+    private String id;
+
+
+    private String name;
+}
--- day-test/src/main/java/com/qilang/test/Test.java
+++ day-test/src/main/java/com/qilang/test/Test.java
@@ -1,5 +1,10 @@
 package com.qilang.test;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,20 +12,51 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Test {
+
+
+    private static final String upperStr="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String lowerStr="abcdefghijklmnopqrstuvwxyz";
+    private static final String numStr="1234567890";
+
+
+
     public static void main(String[] args) {
 
 
-        Map<String, String> map = new HashMap<>();
-        map.put("a", "b");
-        map.put("b", "c");
-        System.out.println(map.keySet());
+//        Map<String, String> map = new HashMap<>();
+//        map.put("a", "b");
+//        map.put("b", "c");
+//        System.out.println(map.keySet());
+//
+//
+//        List<String> list = new ArrayList<>();
+//        Map<String,String> map1 = list.stream().collect(Collectors.toMap(k -> k, v -> v));
+//        List<String> list1 = new ArrayList<>();
+//        list1.add("12");
+//        map1.keySet().removeAll(list1);
+//        System.out.println(map1.keySet());
+        int randomIntA = RandomUtil.randomInt(4, 9);
+        int randomIntB = RandomUtil.randomInt(4, 9);
+
+        String a = RandomUtil.randomString(4);
+        String b = RandomUtil.randomStringUpper(4);
+
+        System.out.println(CharSequenceUtil.appendIfMissing(a, b));
 
 
-        List<String> list = new ArrayList<>();
-        Map<String,String> map1 = list.stream().collect(Collectors.toMap(k -> k, v -> v));
-        List<String> list1 = new ArrayList<>();
-        list1.add("12");
-        map1.keySet().removeAll(list1);
-        System.out.println(map1.keySet());
+//        String randomString="";
+//        while (!(StrUtil.containsAny(randomString, upperStr) && StrUtil.containsAny(randomString,lowerStr) && StrUtil.containsAny(randomString,numStr))){
+//            // 生成8到17之间的随机整数(包含8不包含17)
+//            int randomInt = RandomUtil.randomInt(8, 17);
+//            randomString = RandomUtil.randomString(upperStr + lowerStr + numStr, randomInt);
+//        }
+//        System.out.println(randomString);
+
+
+        Simple simple = new Simple();
+        simple.setId("asasa");
+        simple.setName("9231jsak");
+
+        System.out.println(BeanUtil.beanToMap(simple));
     }
 }
