fb29027e0762f4780d9707329abaec43d83d6afe
“huqilang” <huqilang@itg.com.cn>
2023-03-07 10:55:56 +0800

    委派 更注重层级之间的执行 策略 是平级之间的选择 调用

--- design-pattern/src/main/java/delegator/Leader.java
+++ design-pattern/src/main/java/delegator/Leader.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Leader implements IEmployee {
 
-    
+
     private Map<String, IEmployee> map = new HashMap<>();
 
     public Leader() {
@@ -13,8 +13,15 @@ public class Leader implements IEmployee {
         map.put("bbbb", new ConEmployeeB());
     }
 
+    //策略很像。但是委派更注重层级
+    //策略更注重  平级之间的选择
+
     @Override
     public void doSomething(String task) {
-
+        if (!map.containsKey(task)) {
+            System.out.println("执行不了，leader无权限");
+            return;
+        }
+        map.get(task).doSomething(task);
     }
 }
