2195bd1557d52374b9fa39864142927f626ed0a0
huqilang <985104177@qq.com>
2023-04-19 00:53:21 +0800

    IguomaoConfig

---
+++ day-test/src/main/java/com/qilang/test/Test3.java
@@ -0,0 +1,31 @@
+package com.qilang.test;
+
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
+import com.qilang.test.json.ReqestDTO;
+import java.util.HashMap;
+
+/**
+ * @author huql
+ **/
+public class Test3 {
+
+    public static void main(String[] args) {
+        ReqestDTO reqestDTO = new ReqestDTO();
+        reqestDTO.setBillCode("asasa");
+        System.out.println(JSON.toJSONString(reqestDTO));
+        //转换成key value 形式
+
+        JSON.parseObject(JSON.toJSONString(reqestDTO), HashMap.class).forEach((k, v) -> {
+            System.out.println(k + "=" + v);
+        });
+
+
+        System.out.println(URLUtil.encode(JSON.toJSONString(reqestDTO)));
+        String str = "ea5b29320cb3d15a9883c1fa4654bd02_app=3823532979&_t=1668131644&bill_code=XD221021000709&limit=200&page=1&query_extend=%7B%22encry_address%22%3Atrue%7Dea5b29320cb3d15a9883c1fa4654bd02";
+        System.out.println(SecureUtil.md5(str).toUpperCase());
+
+    }
+}
---
+++ day-test/src/main/java/com/qilang/test/json/ReqestDTO.java
@@ -0,0 +1,16 @@
+package com.qilang.test.json;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * @author huql
+ **/
+@Data
+public class ReqestDTO implements Serializable {
+
+    @JSONField(name = "bill_code")
+    private String billCode;
+
+}
